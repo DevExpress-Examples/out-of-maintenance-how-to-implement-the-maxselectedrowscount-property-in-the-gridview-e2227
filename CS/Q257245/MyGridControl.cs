63d7fb47b969732f2fad4d7122be4c389facce07
TheLeftExit <theleftexit@protonmail.com>
2022-01-26 17:29:38 +0300

    Updated to 18.1.7+

--- CS/Q257245/MyGridControl.cs
+++ CS/Q257245/MyGridControl.cs
@@ -1,16 +1,21 @@
-using DevExpress.XtraGrid;
-using DevExpress.XtraGrid.Views.Grid;
+﻿using DevExpress.XtraGrid;
 using DevExpress.XtraGrid.Registrator;
 using DevExpress.XtraGrid.Views.Base;
-using System.Windows.Forms;
-using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Views.Base.Handler;
 using DevExpress.XtraGrid.Views.Base.ViewInfo;
-using DevExpress.XtraGrid.Views.Grid.ViewInfo;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
 
 namespace DXSample {
-    public class MyGridControl :GridControl {
-        public MyGridControl() : base() { }
+    [ToolboxItem(true)]
+    public class MyGridControl : GridControl {
+        protected override BaseView CreateDefaultView() {
+            return CreateView("MyGridView");
+        }
 
         protected override void RegisterAvailableViewsCore(InfoCollection collection) {
             base.RegisterAvailableViewsCore(collection);
@@ -18,53 +23,46 @@ namespace DXSample {
         }
     }
 
-    public class MyGridView :GridView {
-        public MyGridView() : base() { }
-        public MyGridView(GridControl grid) : base(grid) { }
-
-        internal const string MyGridViewName = "MyGridView";
-        protected override string ViewName { get { return MyGridViewName; } }
-
-        private int fMaxSelectedRowsCount = -1;
-        public int MaxSelectedRowsCount {
-            get { return fMaxSelectedRowsCount; }
-            set {
-                if (fMaxSelectedRowsCount == value) return;
-                fMaxSelectedRowsCount = value;
-                ClearSelection();
-            }
-        }
-
-        protected override void InvertFocusedRowSelectionCore(BaseHitInfo hitInfo) {
-            if (OptionsSelection.MultiSelectMode == GridMultiSelectMode.RowSelect && MaxSelectedRowsCount > 0 && 
-                MaxSelectedRowsCount <= SelectedRowsCount && !IsRowSelected(((GridHitInfo)hitInfo).RowHandle)) 
-                return;
-            base.InvertFocusedRowSelectionCore(hitInfo);
-        }
-
-        protected override void SelectAnchorRangeCore(bool controlPressed, bool allowCells) {
-            if (MaxSelectedRowsCount > 0 && OptionsSelection.MultiSelectMode == GridMultiSelectMode.RowSelect) {
-                int anchor = GetVisibleIndex(SelectionAnchorRowHandle);
-                int focused = GetVisibleIndex(FocusedRowHandle);
-                if (controlPressed) {
-                    int selected = SelectedRowsCount;
-                    int start = Math.Min(anchor, focused);
-                    int end = Math.Max(anchor, focused);
-                    for (int i = start; i < end; i++) if (!IsRowSelected(i)) selected++;
-                    if (selected >= MaxSelectedRowsCount) return;
-                } else if (Math.Abs(anchor - focused) >= MaxSelectedRowsCount) return;
-            }
-            base.SelectAnchorRangeCore(controlPressed, allowCells);
-        }
-    }
-
-    public class MyGridViewInfoRegistrator :GridInfoRegistrator {
-        public MyGridViewInfoRegistrator() : base() { }
-
-        public override string ViewName { get { return MyGridView.MyGridViewName; } }
+    public class MyGridViewInfoRegistrator : GridInfoRegistrator {
+        public override string ViewName => "MyGridView";
 
         public override BaseView CreateView(GridControl grid) {
             return new MyGridView(grid);
+        }
+    }
+
+    public class MyGridView : DevExpress.XtraGrid.Views.Grid.GridView {
+        public MyGridView() {
+        }
+
+        public MyGridView(GridControl grid) : base(grid) {
+        }
+
+        protected override string ViewName => "MyGridView";
+
+        [DefaultValue(-1)]
+        public int MaxSelectedRowsCount { get; set; } = -1;
+
+        public override void SelectRow(int rowHandle) {
+            if (SelectedRowsCount == MaxSelectedRowsCount)
+                return;
+            base.SelectRow(rowHandle);
+        }
+
+        public override void SelectRange(int startRowHandle, int endRowHandle) {
+            if(startRowHandle == SelectionAnchorRowHandle && MaxSelectedRowsCount > -1) {
+                if(endRowHandle >= startRowHandle) {
+                    endRowHandle = Math.Min(endRowHandle, startRowHandle + MaxSelectedRowsCount - 1);
+                } else {
+                    endRowHandle = Math.Max(endRowHandle, startRowHandle - MaxSelectedRowsCount + 1);
+                }
+            }
+            base.SelectRange(startRowHandle, endRowHandle);
+        }
+
+        public override void SelectAll() {
+            if (MaxSelectedRowsCount == -1)
+                base.SelectAll();
         }
     }
 }
